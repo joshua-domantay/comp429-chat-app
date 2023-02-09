3bb296cc961259880a7d908b950a382bdaebd183
Joshua Anthony Domantay <dom.josh2701@gmail.com>
2023-02-08 20:39:23 -0800

    Allow connection with peers using connect. Need to validate address. Messages can now be sent using send function

--- chat.py
+++ chat.py
@@ -9,6 +9,14 @@ from socket import *
 
 port = 0
 connections = []
+curr_id = 1
+
+def valid_port(port):
+    if(port.isdecimal()):
+        n = int(port)
+        if((n >= 0) and (n <= 65535)):
+            return True
+    return False
 
 def help():
     print("myip - Display IP address")
@@ -27,7 +35,17 @@ def myport():
     return port
 
 def connect(dest, port):
-    pass
+    try:
+        # TODO: Test if connection is valid
+        # testSocket = socket(AF_INET, SOCK_STREAM)
+        # testSocket.connect((dest, port))
+
+        # If connection is successful
+        global curr_id
+        connections.append((curr_id, dest, port))
+        curr_id = curr_id + 1
+    except Exception as e:
+        print("Connection error", e)
 
 def list_connections():
     pass
@@ -36,7 +54,15 @@ def terminate(conn_id):
     pass
 
 def send(conn_id, msg):
-    pass
+    for i in range(len(connections)):
+        if(int(connections[i][0]) == conn_id):
+            # Send a message
+            client_socket = socket(AF_INET, SOCK_STREAM)
+            client_socket.connect((connections[i][1], connections[i][2]))
+            client_socket.send(msg.encode())
+            client_socket.close()
+            return
+    print("Connection id not found")
 
 def setup_server(port):
     server_socket = socket(AF_INET, SOCK_STREAM)
@@ -61,8 +87,13 @@ def get_input():
         elif(x[0].lower() == "myport"):
             print("The program runs on port number", myport())
         elif(x[0].lower() == "connect"):
-            # TODO connect()
-            pass
+            if(len(x) == 3):
+                if(valid_port(x[2])):
+                    connect(x[1], int(x[2]))
+                else:
+                    print("Invalid port number")
+            else:
+                print("Please provide the destination and the port number")
         elif(x[0].lower() == "list"):
             # TODO list_connections()
             pass
@@ -70,19 +101,27 @@ def get_input():
             # TODO terminate()
             pass
         elif(x[0].lower() == "send"):
-            # TODO send()
-            pass
+            if(len(x) >= 3):
+                if(x[1].isdigit()):
+                    msg = x[2:]
+                    msg = ' '.join(msg)
+                    send(int(x[1]), msg)
+                else:
+                    print("Invalid connection id")
+            else:
+                print("Please provide the connection id and the message")
         elif(x[0].lower() == "exit"):
             break
         else:
-            pass    # Disregard
+            print(connections)
+            # pass    # Disregard
 
 def main():
     # Get command line arguments
     arg_len = len(sys.argv)
 
     # If arguments contain port number...
-    if((arg_len == 2) and (sys.argv[1].isdecimal())):
+    if((arg_len == 2) and (valid_port(sys.argv[1]))):
         port = int(sys.argv[1])
         try:    # If connection goes through...
             _thread.start_new_thread(setup_server, (port,))     # Thread for listening
