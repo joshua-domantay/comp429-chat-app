0c48182b3b4ab81d9c1e647965a12b91c713bfbd
Joshua Anthony Domantay <dom.josh2701@gmail.com>
2023-02-08 17:07:31 -0800

    Make rough program structure

--- chat.py
+++ chat.py
@@ -2,3 +2,39 @@
 # Professor Senhua Yu
 # COMP 429 - 16938
 # 4 March 2023
+
+from socket import *
+
+def help():
+    pass
+
+def myip():
+    pass
+
+def myport():
+    pass
+
+def connect(dest, port):
+    pass
+
+def list_connections():
+    pass
+
+def terminate(conn_id):
+    pass
+
+def send(conn_id, msg):
+    pass
+
+def exit():
+    pass
+
+# TODO
+# Get command line arguments
+# If arguments contain port number...
+##### Make client socket
+##### If connection goes through...
+######### Thread for server?
+######### Get user input (help, myip, myport, etc.)
+##### Else error message for no connection
+# Else error message for lack of port number
