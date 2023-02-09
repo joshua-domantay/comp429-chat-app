cd33f2cd18e65a8bf8a356edffb028c44e91e6bc
Joshua Anthony Domantay <dom.josh2701@gmail.com>
2023-02-08 19:58:02 -0800

    Add user input and receiving of messages at the same time. Complete functions help(), myip(), myport(), and exit()

--- chat.py
+++ chat.py
@@ -3,16 +3,28 @@
 # COMP 429 - 16938
 # 4 March 2023
 
+import sys
+import _thread
 from socket import *
 
+port = 0
+connections = []
+
 def help():
-    pass
+    print("myip - Display IP address")
+    print("myport - Display program port number")
+    print("connect <destination> <port> - Connect to another peer")
+    print("list - List every connected peer's IP address and port number")
+    print("terminate - Close a connection")
+    print("send <connection id> <message> - Send message to a peer")
+    print("exit - Exit the program")
 
 def myip():
-    pass
+    hostname = gethostname()
+    return gethostbyname(hostname)      # Return ip address
 
 def myport():
-    pass
+    return port
 
 def connect(dest, port):
     pass
@@ -26,15 +38,60 @@ def terminate(conn_id):
 def send(conn_id, msg):
     pass
 
-def exit():
-    pass
+def setup_server(port):
+    server_socket = socket(AF_INET, SOCK_STREAM)
+    server_socket.bind((myip(), port))
+    server_socket.listen(1)
+    while True:
+        conn_socket, addr = server_socket.accept()
+        msg = conn_socket.recv(1024).decode()
+        print("Message received from", addr[0])
+        print("Sender's Port:", addr[1])
+        print("Message:", msg)
+        conn_socket.close()
 
-# TODO
-# Get command line arguments
-# If arguments contain port number...
-##### Make client socket
-##### If connection goes through...
-######### Thread for server?
-######### Get user input (help, myip, myport, etc.)
-##### Else error message for no connection
-# Else error message for lack of port number
+def get_input():
+    while True:
+        x = input(">> ")
+        x = x.split()
+        if(x[0].lower() == "help"):
+            help()
+        elif(x[0].lower() == "myip"):
+            print("The IP address is", myip())
+        elif(x[0].lower() == "myport"):
+            print("The program runs on port number", myport())
+        elif(x[0].lower() == "connect"):
+            # TODO connect()
+            pass
+        elif(x[0].lower() == "list"):
+            # TODO list_connections()
+            pass
+        elif(x[0].lower() == "terminate"):
+            # TODO terminate()
+            pass
+        elif(x[0].lower() == "send"):
+            # TODO send()
+            pass
+        elif(x[0].lower() == "exit"):
+            break
+        else:
+            pass    # Disregard
+
+def main():
+    # Get command line arguments
+    arg_len = len(sys.argv)
+
+    # If arguments contain port number...
+    if((arg_len == 2) and (sys.argv[1].isdecimal())):
+        port = int(sys.argv[1])
+        try:    # If connection goes through...
+            _thread.start_new_thread(setup_server, (port,))     # Thread for listening
+            get_input()
+        except:     # Else error message for no connection
+            print("Connection error")
+            return
+    else:   # Else error message for lack of port number
+        print("Please provide a port number on your arguments")
+
+if __name__ == "__main__":
+    sys.exit(main())
